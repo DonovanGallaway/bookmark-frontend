f8c5581ff383651109fbbe8ab7085397ca833ca4
Mitchell Paoletti <mitchellp0709@gmail.com>
2021-11-11 19:14:39 -0500

    added routes to app.js for much more clarity

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import './App.css';
 import Header from './components/Header';
 import Index from './pages/Index';
 import New from './pages/New';
+import { Route, Routes } from 'react-router-dom'
 
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Index/>
-      <New />
-      
+
+
+      <Routes>
+
+        <Route path="/" element={<Index/>}/>
+        <Route path="/new" element={ <New/>}/>
+
+      </Routes>
     </div>
   );
 }
